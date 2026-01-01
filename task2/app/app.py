556486c359e4347aa9f4a38f4204b6443dbe4c99
Abdiyasir <AbdiyasirM100@gmail.com>
2026-01-01 18:34:38 +0000

    task2

--- task2/app/app.py
+++ task2/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+import app as flask_app
 import redis
 
 
@@ -21,3 +21,4 @@ def count():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
+    
--- task2/app/test_app.py
+++ task2/app/test_app.py
@@ -1,5 +1,6 @@
-import pytest
 from unittest.mock import MagicMock
+
+import pytest
 
 import app as flask_app
 
@@ -17,7 +18,7 @@ def test_welcome_route(client):
     assert response.data.decode() == "Welcome to my Coderco Docker Project!"
 
 
-def test_count_route(client, monkeypatch):
+def test_count_route_increments_and_returns_value(client, monkeypatch):
     mock_redis = MagicMock()
     mock_redis.incr.return_value = 1
 
@@ -26,5 +27,5 @@ def test_count_route(client, monkeypatch):
     response = client.get("/count")
     assert response.status_code == 200
     assert response.data.decode() == "You are Visitor number: 1"
-
     mock_redis.incr.assert_called_once_with("visitor_count")
+    
