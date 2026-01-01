0aa9b2d26635ce4f01c9e65a19651b35c225a205
Abdiyasir <AbdiyasirM100@gmail.com>
2026-01-01 18:53:56 +0000

    TASK2

--- task2/app/test_app.py
+++ task2/app/test_app.py
@@ -8,8 +8,8 @@ import app as flask_app
 @pytest.fixture
 def client():
     flask_app.app.config["TESTING"] = True
-    with flask_app.app.test_client() as client:
-        yield client
+    with flask_app.app.test_client() as test_client:
+        yield test_client
 
 
 def test_welcome_route(client):
@@ -18,7 +18,7 @@ def test_welcome_route(client):
     assert response.data.decode() == "Welcome to my Coderco Docker Project!"
 
 
-def test_count_route_increments_and_returns_value(client, monkeypatch):
+def test_count_route_increments_visitor_count(client, monkeypatch):
     mock_redis = MagicMock()
     mock_redis.incr.return_value = 1
 
@@ -27,4 +27,6 @@ def test_count_route_increments_and_returns_value(client, monkeypatch):
     response = client.get("/count")
     assert response.status_code == 200
     assert response.data.decode() == "You are Visitor number: 1"
+
     mock_redis.incr.assert_called_once_with("visitor_count")
+    
